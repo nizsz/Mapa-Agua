35e22b9bf00705fbcabba5a358d56b385d6f2990
nizsz <PohaDeCla@gmail.com>
2026-09-13 22:24:10 -0300

    Add login DTO

---
+++ src/main/java/com/mapaagua/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.mapaagua.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDto(
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        String senha
+) {
+}
